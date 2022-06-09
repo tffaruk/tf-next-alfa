94ad2968619fab3ab111f4185e31f1a480689f02
tffaruk <faruk.themefisher@gmail.com.com>
2022-06-09 21:41:03 +0600

    add functionality

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ const Home = ({ homePageData, products, productIndex, allDownloadData }) => {
 
 export default Home;
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const homePageData = getSingleFile("content/_index.md");
   const allProducts = getAllData("content/products", false);
   const productIndex = getSingleFile("content/products/_index.md");
